e527da24e4a6aa215229b82455da7348e90c51cc
Evgeny.Shinkarev@urfu.me <Evgeny.Shinkarev@urfu.me>
2021-12-05 13:58:45 +0500

    Исправил 1 и 3 задачу. С 4 всё плохо.

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -14,37 +14,38 @@
 }
 
 export class Currency{
-    private static readonly _selector: {[name: string]: string[]} = {
-        [CurrencyType.Material]: ["Dollar", "Ruble"],
-        [CurrencyType.Crypto]: ["Etherium", "XRP"],
-        [CurrencyType.Metal]: ["Gold"]
-    }
     public readonly name: string;
     public readonly unit: string;
     public readonly type: CurrencyType;
     private _value: number;
+    private static readonly _selector: {[s: string]: CurrencyType} = {
+        "dollar": CurrencyType.Material,
+        "ruble": CurrencyType.Material,
+        "xrp": CurrencyType.Crypto,
+        "etherium": CurrencyType.Crypto,
+        "gold": CurrencyType.Metal
+    }
     public get value(): number{
         return this._value;
     }
     public set value(n: number){
-        if (n < 0){
+        if (!isFinite(n) || n < 0){
             throw new Error(`value can't be less 0, got ${n}`);
         }
         this._value = n;
     }
     
     constructor(name: string, value = 0, unit: string){
-        if (name?.length === 0 || unit.length === 0){
+        if (!this.isStringCorrect(name) || !this.isStringCorrect(unit)){
             throw new Error("required not empty name or unit");
         }
-        for (const i in Currency._selector){
-            if (name in Currency._selector[i]){
-                this.type = parseInt(i);
-                break;
-            }
-        }
         this.name = name;
+        this.type = Currency._selector[name.toLowerCase() as keyof typeof Currency._selector];
         this.unit = unit;
         this.value = value;
+    }
+
+    private isStringCorrect(s: string){
+        return s && typeof s === "string"
     }
 }
--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -6,37 +6,39 @@
 */
 import { Currency } from "../task_1";
 
-export class Vault implements ISecureVaultRequisites{
+export class Vault implements ISecureVaultRequisites {
 	public id: number;
 	public store: Set<Currency> = new Set<Currency>()
-	public withdraw(c: Currency){
+	public withdraw(c: Currency): void {
 		let hasCurrency = false;
-		this.store.forEach(x => {
+		for (const x of Array.from(this.store)) {
 			if (x.name === c.name) {
 				if (c.value > x.value) {
 					throw new Error("The loan denied");
 				}
 				x.value -= c.value;
 				hasCurrency = true;
+				break;
 			}
-		});
-		if (!hasCurrency){
+		}
+		if (!hasCurrency) {
 			throw new Error(`store doesn't contain ${c.name}`);
 		}
 	}
-	public deposit(c: Currency){
+	public deposit(c: Currency): void{
 		let hasCurrency = false;
-		this.store.forEach(x => {
+		for(const x of Array.from(this.store)) {
 			if (x.name === c.name) {
 				x.value += c.value;
 				hasCurrency = true;
+				break;
 			}
-		});
+		}
 		if (!hasCurrency){
 			this.store.add(c);
 		}
 	}
-	public transfer(c: Currency, v: Vault){
+	public transfer(c: Currency, v: Vault): void{
 		this.withdraw(c);
 		v.deposit(c);
 	}
